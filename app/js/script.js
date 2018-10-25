02f2690cf1d24e6ea3d8ec49c19e66e729710ac7
sonrisacc <sonrisa@dronedeploy.com>
2018-10-25 16:03:49 -0700

    Clean up files and add toggle

--- app/js/script.js
+++ app/js/script.js
@@ -3,71 +3,7 @@ const dronedeploy = new DroneDeploy({version: 1});
 
 // Name of the function to call. Name should be the same set in serverless.yml
 const FUNCTION_NAME = "volume-viz";
-// const functionPaths = {
-//   store: 'store'
-// }
 
-/**
- * Sets the url value to the DroneDeploy Datastore
- */
-async function onSave() {
-  disableSaveButton();
-  const iftttUrl = getUrl();
-  let response = await setStoredUrl(iftttUrl);
-  const status = response.status;
-  const text = await response.text();
-  console.log(status);
-  if (status != 200) {
-    console.log(`response from datastore | status: ${status}, message: ${text}`);
-    let errorMessage = `An error occurred, please try again. <br> Error message: ${text}`;
-    hide(controls.dataPage);
-    hide(controls.loadingPage);
-    setErrorMessage(errorMessage);
-    show(controls.errorPage);
-  } else {
-    showDataPageMessage(controls.dataMessage, 'URL was successfully saved.');
-  }
-  resetSaveButton();
-}
-
-/**
- * Generic function for calling out to Datastore
- */
-async function callStoreFunction(options) {
-  const api = await dronedeploy;
-
-  // Grabs the URL of the function to call by name of function
-  const functionUrl = await api.Functions.getUrl(FUNCTION_NAME);
-
-  // Get a token to ensure auth when calling your function
-  const token = await api.Authorization.getScopedToken();
-  console.log(token);
-  options.headers = {
-    'Authorization': `Bearer ${token}`
-  }
-  return fetch(`${functionUrl}/${functionPaths.store}`, options);
-}
-
-/**
- * Sets the url value to the DroneDeploy Datastore
- */
-async function setStoredUrl(url) {
-  const options = {
-    method: "POST",
-    body: JSON.stringify({
-      endpoint: url
-    })
-  }
-  return await callStoreFunction(options);
-}
-
-/**
- * Retrives the stored url value if any
- */
-async function getStoredUrl() {
-  const options = {};
-  return await callStoreFunction(options);
-}
 
 /**
  * Actions to take when a user expands the app
@@ -78,6 +14,7 @@ async function getStoredUrl() {
 /**
  * Generic function for calling out to Function
  */
+
 async function callFunction() {
   const api = await dronedeploy;
 
@@ -97,97 +34,48 @@ async function callFunction() {
   // return fetch(`${functionUrl}`, options);
 }
 async function onExpand() {
-  console.log('wrokming');
-  callFunction();
+  console.log('working');
+  // callFunction();
 
 }
 
-/**
- * ----------------------------------- UI Controls ------------------------------------------------
- */
-let isExpanded = false;
-const upArrow = 'https://s3.amazonaws.com/drone-deploy-plugins/templates/login-example-imgs/arrow-up.svg';
-const downArrow = 'https://s3.amazonaws.com/drone-deploy-plugins/templates/login-example-imgs/arrow-down.svg';
-const expandArrow = document.querySelector('.expand-arrow');
-const expandBody = document.querySelector('.expand-section');
-const expandRow = document.querySelector('.expand-row');
-const controls = {
-  url: '#url',
-  loadingPage: '#loading-page',
-  dataPage: '#data-page',
-  dataMessage: '#data-message',
-  errorPage: '#error-page',
-  errorMessage: '#error-message',
-  saveButton: '#save-button'
-}
 
-expandRow.addEventListener('click', function() {
-  isExpanded = !isExpanded
-  if (isExpanded){
-    expandArrow.src = upArrow;
-    expandBody.style.display = 'block';
-    onExpand();
-  } else{
-    expandArrow.src = downArrow;
-    expandBody.style.display = 'none';
-    onCollapse();
+
+/* ****************************************************************************
+    Toggle control section
+   **************************************************************************** */
+const toggle = document.querySelector('dd-toggle');
+const planRow = document.querySelector('.plan-hidden-rows');
+const toggleControls = function (on) {
+  if (on) {
+    planRow.style.display = 'block';
+
+  } else {
+    planRow.style.display = 'none';
   }
+}
+toggle.addEventListener('change', function (e) {
+  toggleControls(e.target.value);
 });
 
-function onCollapse() {
-  hide(controls.loadingPage);
-  hide(controls.dataPage);
-}
+/* ****************************************************************************
+    Expandable Section
+   **************************************************************************** */
 
-function getUrl() {
-  const select = document.querySelector(controls.url);
-  return select.value;
-}
+var isExpanded = false;
+var upArrow = 'https://s3.amazonaws.com/drone-deploy-plugins/templates/login-example-imgs/arrow-up.svg';
+var downArrow = 'https://s3.amazonaws.com/drone-deploy-plugins/templates/login-example-imgs/arrow-down.svg';
+var expandArrow = document.querySelector('.expand-arrow');
+var expandBody = document.querySelector('.expand-section');
+var expandRow = document.querySelector('.expand-row');
 
-function setUrl(url) {
-  const select = document.querySelector(controls.url);
-  select.value = url;
-  Materialize.updateTextFields();
-}
-
-function setElementsDisplay(display, ...selectors) {
-  selectors.forEach((selector) => document.querySelector(selector).style.display = display);
-}
-
-function setElementsInnerHtml(value, ...selectors) {
-  selectors.forEach((selector) => document.querySelector(selector).innerHTML = value);
-}
-
-function setErrorMessage(errorMessage) {
-  setElementsInnerHtml(errorMessage, controls.errorMessage);
-}
-
-function disableSaveButton() {
-  setElementsInnerHtml('Saving', controls.saveButton);
-  document.querySelector(controls.saveButton).disabled = true;
-}
-
-function resetSaveButton() {
-  setElementsInnerHtml('Save', controls.saveButton);
-  document.querySelector(controls.saveButton).disabled = false;
-}
-
-function showDataPageMessage(selector, message) {
-  document.querySelector(selector).innerHTML = message;
-  document.querySelector(selector).style.display = '';
-  setTimeout(() => {
-    document.querySelector(selector).style.display = 'none';
-  }, 3000);
-}
-
-function show(...selectors) {
-  setElementsDisplay('', ...selectors);
-}
-
-function hide(...selectors) {
-  setElementsDisplay('none', ...selectors);
-}
-
-function hideAllPages() {
-  setElementsDisplay('none', '.page');
-}
+expandRow.addEventListener('click', function () {
+  isExpanded = !isExpanded
+  if (isExpanded) {
+    expandArrow.src = upArrow;
+    expandBody.style.display = 'block';
+  } else {
+    expandArrow.src = downArrow;
+    expandBody.style.display = 'none';
+  }
+});
